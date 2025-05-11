a40d39d4392fab9c73ac7b9d85c2245305daeeba
shash-786 <ssalian60@yahoo.com>
2025-05-11 18:41:53 +0530

    added some components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <app-header />
+
+    <main>
+      <app-home />
+    </main>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `,
+  ],
 })
+
 export class AppComponent {
   title = 'wordle';
 }
---
+++ src/app/components/greeting/greeting.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greeting',
+  imports: [],
+  templateUrl: './greeting.component.html',
+  styleUrl: './greeting.component.css'
+})
+export class GreetingComponent {
+
+}
